e0a5ee62b1e43a2e0229dc6a6bf2b8071b9c574a
Mert Bozkurtlar <bozkurtlarmert@gmail.com>
2024-12-11 16:40:45 +0300

    Add MenuBar component and placeholder views for Home, Profile, and Quests

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fixed bottom-[4%] h-[4rem] w-full px-[4%]">
+    <div
+      class="h-full w-full bg-black rounded-2xl flex items-center justify-around"
+    >
+      <div class="flex flex-col h-full items-center justify-center">
+        <i
+          class="pi pi-question-circle"
+          style="color: white; font-size: 1.5rem"
+        ></i>
+        <h1 class="text-white text-sm">Quests</h1>
+      </div>
+      <div class="flex flex-col items-center">
+        <i class="pi pi-home" style="color: white; font-size: 1.5rem"></i>
+        <h1 class="text-white text-sm">Home</h1>
+      </div>
+      <div class="flex flex-col items-center">
+        <i class="pi pi-user" style="color: white; font-size: 1.5rem"></i>
+        <h1 class="text-white text-sm">Profile</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-screen h-screen bg-white">
+    PLACEHOLDER PROFILE
+    <MenuBar />
+  </div>
+</template>
+
+<script setup>
+import MenuBar from '@/components/menuBar.vue'
+</script>
+
+<style lang="scss" scoped></style>
