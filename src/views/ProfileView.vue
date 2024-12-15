17365b5293a058f9a19e8731a36ef5fc305e1537
Mert Bozkurtlar <bozkurtlarmert@gmail.com>
2024-12-15 18:40:50 +0300

    Profile View Code Refactoring

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,69 +1,69 @@
 <template>
-  <div
-    class="w-full h-full"
-    style="
-      background-image: url(blue_honeycomb.png);
-      background-size: contain;
-      background-color: #31556e;
-      padding-top: 24px;
-    "
-  >
-    <div class="">
-      <!-- PROFILE SECTION -->
-      <div class="p-5 flex flex-col gap-4">
-        <div class="flex items-center justify-between z-10 relative">
-          <h1 class="text-white text-2xl">{{ profileData.name }}</h1>
-          <RouterLink to="/settings" class="flex items-center justify-end">
-            <i class="pi-cog pi text-2xl text-white"></i>
-          </RouterLink>
-        </div>
-        <div class="flex items-center gap-5 z-10 relative">
-          <img
-            class="w-24 aspect-square object-cover rounded-xl"
-            :src="profileData.profilePhoto"
-          />
-          <div class="flex flex-col justify-between grow">
-            <div class="flex gap-3 max-w-60">
-              <div class="flex flex-col grow justify-center items-center">
-                <p class="text-white text-xl font-semibold">
-                  {{ profileData.postCount }}
-                </p>
-                <p class="text-white text-s">Posts</p>
-              </div>
-              <div class="flex flex-col grow justify-center items-center">
-                <p class="text-white text-xl font-semibold">
-                  {{ profileData.friendCount }}
-                </p>
-                <p class="text-white text-s">Friends</p>
-              </div>
-              <div class="flex flex-col grow justify-center items-center">
-                <p class="text-white text-xl font-semibold">
-                  {{ profileData.badgeCount }}
-                </p>
-                <p class="text-white text-s">Badges</p>
-              </div>
-            </div>
+  <div class="w-full h-full bg-[#31556e]">
+    <!-- PROFILE SECTION -->
+    <div
+      class="px-4 pt-10 pb-4 flex flex-col gap-4"
+      :style="{
+        backgroundImage: backgroundImage,
+        backgroundBlendMode: 'luminosity',
+        backgroundPosition: 'center',
+        backgroundColor: '#31556e',
+      }"
+    >
+      <!-- Name and Settings Icons -->
+      <div class="flex items-center justify-between">
+        <h1 class="text-white text-2xl">{{ profileData.name }}</h1>
+        <RouterLink to="/settings" class="flex items-center justify-end">
+          <i class="pi-cog pi text-2xl text-white"></i>
+        </RouterLink>
+      </div>
+      <!-- Profile Photo and Stats -->
+      <div class="flex items-center justify-between">
+        <img
+          class="w-24 aspect-square object-cover rounded-xl"
+          :src="profileData.profilePhoto"
+        />
+        <div class="w-full flex items-center justify-evenly">
+          <div class="flex flex-col items-center">
+            <p class="text-white text-xl font-semibold">
+              {{ profileData.postCount }}
+            </p>
+            <p class="text-white text-s">Posts</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <p class="text-white text-xl font-semibold">
+              {{ profileData.friendCount }}
+            </p>
+            <p class="text-white text-s">Friends</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <p class="text-white text-xl font-semibold">
+              {{ profileData.badgeCount }}
+            </p>
+            <p class="text-white text-s">Badges</p>
           </div>
         </div>
       </div>
-      <!-- POSTS SECTION -->
-      <div class="bg-white rounded-t-3xl p-5 pt-5 grid grid-cols-2 gap-4">
-        <div v-for="(post, index) in profileData.posts" v-bind:key="index">
-          <img
-            :src="post.post_image"
-            class="object-cover w-full h-full rounded-2xl"
-          />
-        </div>
-      </div>
     </div>
-    <MenuBar />
+    <!-- POSTS SECTION -->
+    <div class="bg-white w-full rounded-t-2xl px-4 pt-4 grid grid-cols-3 gap-1">
+      <img
+        v-for="(post, index) in profileData.posts"
+        v-bind:key="index"
+        :src="post.post_image"
+      />
+    </div>
   </div>
+  <MenuBar />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
 import MenuBar from '@/components/MenuBar.vue'
+
+import honeycomb from '@/assets/honeycomb.png'
+const backgroundImage = `url(${honeycomb})`
 
 const profileData = ref({
   name: 'Susan Clay',
