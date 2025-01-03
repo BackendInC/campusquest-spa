d2968dfbd1a6cc0016beb12c9d9abee276cca81f
Mert Bozkurtlar <bozkurtlarmert@gmail.com>
2025-01-03 10:19:18 +0300

    Quest functionality

--- src/stores/quests.js
+++ src/stores/quests.js
@@ -1,81 +1,104 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
+import { useFetch } from '@vueuse/core'
+
+/**
+ * Store for managing quests.
+ *
+ * @typedef {Object} Quest
+ * @property {string} name - The name of the quest.
+ * @property {string} description - Description of the quest.
+ * @property {string|null} image_url - URL of the image associated with the quest.
+ * @property {boolean} is_complete - Indicates if the quest is completed.
+ * @property {boolean} is_verified - Indicates if the quest is verified.
+ * @property {number|null} post_id - The ID of the post associated with the quest.
+ * @property {number} quest_id - The ID of the quest.
+ *
+ * @example
+ * {
+ *   id: 6,
+ *   name: "The Famous Lake",
+ *   description: "Take a tour around the Lake in the Ayazaga Campus, and post a picture of the most unique bug you encounter!",
+ *   image_url: null,
+ *   is_complete: false,
+ *   is_verified: false,
+ *   post_id: null,
+ *   quest_id: 6
+ * }
+ **/
 
 export const useQuestsStore = defineStore('quests', () => {
-  const quests = ref([
-    {
-      id: 1,
-      title: 'Find the Hidden Pond',
-      details:
-        "Explore the campus to discover the hidden pond near the science building. It's a peaceful spot, perfect for some quiet time.",
-      status: 'active',
-    },
-    {
-      id: 2,
-      title: 'Library Challenge',
-      details:
-        'Visit the library and find a book on your favorite subject. Take a picture of it and show it to the librarian to complete this quest.',
-      status: 'pending',
-    },
-    {
-      id: 3,
-      title: 'Campus Fitness Walk',
-      details:
-        'Walk around the entire campus and snap a selfie in front of the main statue. Share the picture in the group to earn points.',
-      status: 'completed',
-    },
-    {
-      id: 4,
-      title: "Professor's Secret",
-      details:
-        'Ask a professor about a topic they are passionate about, and write down the most interesting thing you learned. Share it with your classmates.',
-      status: 'active',
-    },
-    {
-      id: 5,
-      title: 'Art Gallery Hunt',
-      details:
-        'Find three pieces of art on campus that you find inspiring. Take a photo with each and add them to your quest report.',
-      status: 'active',
-    },
-    {
-      id: 6,
-      title: 'Community Service',
-      details:
-        'Volunteer at the community center for at least one hour. Snap a picture while helping out and share it with the group to complete the quest.',
-      status: 'active',
-    },
-  ])
+  const quests = ref([])
 
-  async function fetchQuests() {
+  async function fetchQuests(jwt) {
     // TODO: Fetch quests from the server
     // and update the quests ref
-    isLoading.value = true
-    isLoading.value = false
+    const { isFetching, error, data } = await useFetch(
+      import.meta.env.VITE_API_URL + '/questsuser',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+      },
+    )
+    if (!error.value) {
+      quests.value = JSON.parse(data.value)
+    }
   }
 
-  // A ref to track if the data is loading
-  const isLoading = ref(false)
-
   // A ref to filter quests by status
-  const display_filter = ref('all')
+  const display_filter = ref('all') // 'all', 'completed', 'active'
 
   // Getter to display quests based on the filter
   const quests_displayed = computed(() => {
     if (display_filter.value === 'all') return quests.value
-    return quests.value.filter(e => {
-      return e.status === display_filter.value
-    })
+    else if (display_filter.value === 'active') {
+      return quests.value.filter(e => {
+        return e.is_complete == false
+      })
+    } else {
+      return quests.value.filter(e => {
+        return e.is_complete
+      })
+    }
   })
 
-  function submitQuest(quest_id, image) {
-    quests.value = quests.value.map(e => {
-      if (e.id === quest_id) {
-        e.completed = true
-        e.image = image.webPath
-      }
-      return e
-    })
+  async function submitQuest(jwt, quest_id, image, caption) {
+    const file = DataURIToBlob(image)
+    const formData = new FormData()
+    formData.append('caption', caption)
+    formData.append('quest_id', quest_id)
+    formData.append('image', file)
+
+    const { isFetching, error, data } = await useFetch(
+      import.meta.env.VITE_API_URL + '/posts',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+        body: formData,
+      },
+    )
+    if (!error.value) {
+      fetchQuests(jwt)
+    }
+  }
+
+  function DataURIToBlob(dataURI) {
+    const splitDataURI = dataURI.split(',')
+    const byteString =
+      splitDataURI[0].indexOf('base64') >= 0
+        ? atob(splitDataURI[1])
+        : decodeURI(splitDataURI[1])
+    const mimeString = splitDataURI[0].split(':')[1].split(';')[0]
+
+    const ia = new Uint8Array(byteString.length)
+    for (let i = 0; i < byteString.length; i++) ia[i] = byteString.charCodeAt(i)
+
+    return new Blob([ia], { type: mimeString })
   }
 
   return { quests, display_filter, quests_displayed, fetchQuests, submitQuest }
--- src/views/QuestsView.vue
+++ src/views/QuestsView.vue
@@ -11,96 +11,94 @@
       <h1 class="text-2xl font-bold text-white">Quests</h1>
     </div>
 
+    <!-- Quests Upload Dialog -->
+    <Dialog v-model:visible="showSubmitDialog" modal header="Post">
+      <h1>Your submission looks great! Now add a caption for it:</h1>
+      <InputText v-model="caption" placeholder="Caption" />
+      <Button @click="async () => await postQuest()">Post</Button>
+    </Dialog>
+
     <!-- Quest List -->
-    <main class="container mx-auto px-4">
-      <div class="flex mb-3">
-        <Button
-          @click="questsStore.display_filter = 'all'"
-          :class="[
-            questsStore.display_filter === 'all'
-              ? 'bg-emerald-500'
-              : 'bg-gray-500',
-          ]"
-          class="text-white text-sm font-medium px-4 py-0 rounded-xl ml-1 h-8"
-        >
-          All
-        </Button>
-        <Button
-          @click="questsStore.display_filter = 'active'"
-          :class="[
-            questsStore.display_filter === 'active'
-              ? 'bg-emerald-500'
-              : 'bg-gray-500',
-          ]"
-          class="text-white text-sm font-medium px-4 py-0 rounded-xl ml-1 h-8"
-        >
-          Active
-        </Button>
-        <Button
-          @click="questsStore.display_filter = 'pending'"
-          :class="[
-            questsStore.display_filter === 'pending'
-              ? 'bg-emerald-500'
-              : 'bg-gray-500',
-          ]"
-          class="text-white text-sm font-medium px-4 py-0 rounded-xl ml-1 h-8"
-        >
-          Pending
-        </Button>
-        <Button
-          @click="questsStore.display_filter = 'completed'"
-          :class="[
-            questsStore.display_filter === 'completed'
-              ? 'bg-emerald-500'
-              : 'bg-gray-500',
-          ]"
-          class="text-white text-sm font-medium px-4 py-0 rounded-xl ml-1 h-8"
-        >
-          Completed
-        </Button>
-      </div>
+    <Suspense>
+      <template #fallback>Loading...</template>
+      <main class="container mx-auto px-4">
+        <div class="flex mb-3">
+          <Button
+            @click="questsStore.display_filter = 'all'"
+            :class="[
+              questsStore.display_filter === 'all'
+                ? 'bg-emerald-500'
+                : 'bg-gray-500',
+            ]"
+            class="text-white text-sm font-medium px-4 py-0 rounded-xl ml-1 h-8"
+          >
+            All
+          </Button>
+          <Button
+            @click="questsStore.display_filter = 'active'"
+            :class="[
+              questsStore.display_filter === 'active'
+                ? 'bg-emerald-500'
+                : 'bg-gray-500',
+            ]"
+            class="text-white text-sm font-medium px-4 py-0 rounded-xl ml-1 h-8"
+          >
+            Active
+          </Button>
+          <Button
+            @click="questsStore.display_filter = 'completed'"
+            :class="[
+              questsStore.display_filter === 'completed'
+                ? 'bg-emerald-500'
+                : 'bg-gray-500',
+            ]"
+            class="text-white text-sm font-medium px-4 py-0 rounded-xl ml-1 h-8"
+          >
+            Completed
+          </Button>
+        </div>
 
-      <Accordion value="null">
-        <AccordionPanel
-          v-for="quest in questsStore.quests_displayed"
-          :key="quest.id"
-          :value="quest.id"
-        >
-          <AccordionHeader>
-            <img
-              :style="{
-                filter:
-                  quest.status === 'completed'
+        <Accordion value="null">
+          <AccordionPanel
+            v-for="quest in questsStore.quests_displayed"
+            :key="quest.quest_id"
+            :value="quest.quest_id"
+          >
+            <AccordionHeader>
+              <img
+                :style="{
+                  filter: quest.is_complete
                     ? 'brightness(100%) sepia(100%) saturate(500%) hue-rotate(45deg)'
-                    : quest.status === 'pending'
-                      ? 'brightness(100%) sepia(100%) saturate(500%) hue-rotate(240deg)'
-                      : '',
-              }"
-              class="h-6 w-6"
-              src="@/assets/quest_icon.png"
-            />
-            <span
-              :class="{ 'line-through': quest.status === 'completed' }"
-              class="text-xl font-bold"
-              >{{ quest.title }}</span
-            >
-          </AccordionHeader>
-          <AccordionContent>
-            <div class="flex flex-col gap-4">
-              <p>{{ quest.details }}</p>
-              <Button
-                class="bg-blue-400"
-                @click="send_quest(quest.id)"
-                v-if="!quest.completed"
-                >Submit</Button
+                    : '',
+                }"
+                class="h-6 w-6"
+                src="@/assets/quest_icon.png"
+              />
+              <span
+                :class="{ 'line-through': quest.is_complete }"
+                class="text-xl font-bold"
+                >{{ quest.name }}</span
               >
-              <img v-if="quest.completed" :src="quest.image" />
-            </div>
-          </AccordionContent>
-        </AccordionPanel>
-      </Accordion>
-    </main>
-
+            </AccordionHeader>
+            <AccordionContent>
+              <div class="flex flex-col gap-4">
+                <p>{{ quest.description }}</p>
+                <Button
+                  class="bg-blue-400"
+                  @click="send_quest(quest.quest_id)"
+                  v-if="!quest.is_complete"
+                  >Submit</Button
+                >
+                <img
+                  v-if="quest.is_complete"
+                  :src="`${backendURL}${quest.image_url}`"
+                />
+              </div>
+            </AccordionContent>
+          </AccordionPanel>
+        </Accordion>
+      </main>
+    </Suspense>
     <MenuBar />
   </div>
 </template>
@@ -112,25 +110,61 @@ import AccordionPanel from 'primevue/accordionpanel'
 import AccordionHeader from 'primevue/accordionheader'
 import AccordionContent from 'primevue/accordioncontent'
 import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import InputText from 'primevue/inputtext'
+import { onBeforeMount, ref } from 'vue'
 import { Camera, CameraResultType } from '@capacitor/camera'
 import { useQuestsStore } from '@/stores/quests'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+
+onBeforeMount(async () => {
+  await questsStore.fetchQuests(authStore.userData.jwt)
+  console.log(questsStore.quests)
+})
 
 import honeycomb from '@/assets/honeycomb.png'
 const backgroundImage = `url(${honeycomb})`
 
 const questsStore = useQuestsStore()
+const backendURL = ref(import.meta.env.VITE_API_URL)
 
-function send_quest(quest_id) {
+// Quest post / submit
+const showSubmitDialog = ref(false)
+const caption = ref('')
+const lastTakenImage = ref('')
+const lastQuestId = ref('')
+
+async function send_quest(quest_id) {
+  console.log(quest_id)
+  lastQuestId.value = quest_id
   takePicture().then(image => {
-    questsStore.submitQuest(quest_id, image)
+    lastTakenImage.value = 'data:image/jpeg;base64,' + image.base64String
+    showSubmitDialog.value = true
   })
+}
+
+async function postQuest() {
+  console.log(
+    lastQuestId.value,
+    caption.value,
+    lastTakenImage.value.base64String,
+  )
+  await questsStore.submitQuest(
+    authStore.userData.jwt,
+    lastQuestId.value,
+    lastTakenImage.value,
+    caption.value,
+  )
+  showSubmitDialog.value = false
 }
 
 const takePicture = async () => {
   const image = await Camera.getPhoto({
     quality: 90,
     allowEditing: true,
-    resultType: CameraResultType.Uri,
+    resultType: CameraResultType.Base64,
   })
 
   return image
